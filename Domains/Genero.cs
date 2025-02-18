4597a7c1ffa77773f82d7a4ba929ede8c81bd9f9
Isabelle Rodrigues <isabelle.r.reis@aluno.senai.br>
2025-02-18 16:54:39 -0300

    Adicionar arquivos de projeto.

---
+++ Domains/Genero.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_filmes_senai.Domains
+{
+    public class Genero
+    {
+        [Key]
+        public Guid IdGenero { get; set; }
+
+        [Column(TypeName = "VARCHAR(30)")]
+
+        [Required(ErrorMessage = "Nome do Genero é obrigatóriio!")]
+
+        public string? Nome { get; set; }
+    }
+}
